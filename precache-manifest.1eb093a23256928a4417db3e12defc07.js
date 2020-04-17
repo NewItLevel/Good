d5bf768183c34a44d80fcc9e600879156fb876dc
NewItLevel <https://github.com/NewItLevel/ItChat.git>
2020-04-17 09:02:48 +0300

    all files

---
+++ precache-manifest.1eb093a23256928a4417db3e12defc07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fb5e51ea5156e9530497a600832413",
+    "url": "/SeriyVoz/index.html"
+  },
+  {
+    "revision": "da440152d929f26ad3f0",
+    "url": "/SeriyVoz/static/css/main.459aa3e5.chunk.css"
+  },
+  {
+    "revision": "659304e221bac6d804bf",
+    "url": "/SeriyVoz/static/js/2.0669c008.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SeriyVoz/static/js/2.0669c008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da440152d929f26ad3f0",
+    "url": "/SeriyVoz/static/js/main.32d084fe.chunk.js"
+  },
+  {
+    "revision": "f61e6c334da0aa938c7f",
+    "url": "/SeriyVoz/static/js/runtime-main.a29e562e.js"
+  }
+]);
